85a975387cc7a5ac06663e0e54e3c5c735b175d2
estasney <estasney@users.noreply.github.com>
2018-02-15 00:00:25 -0500

    d3 updates

--- mysite/app_folder/routes.py
+++ mysite/app_folder/routes.py
@@ -14,39 +14,44 @@ def index():
 def data():
     """
     On request, this returns origins and targets of a lookup term
-
     """
 
     term = request.args.get('term')
+
+    data = []  # Holds response
+
     if term:
         pass
     else:
         abort(401)
+
     limit = request.args.get('limit')
     if limit:
         limit = int(limit)
-
+    else:
+        limit = 3
 
     # Locate skill object
     skill = Skill.query.filter_by(name=term).first()
-
-    # Data as origin
-    origin_data = skill.get_origins_data()
-    # Sorted
-    origin_data = sorted(origin_data, key=itemgetter(1), reverse=True)
-
-    if limit:
-        origin_data = origin_data[:limit]
+    origin_data = sorted(skill.get_origins_data(), key=itemgetter(1), reverse=True)[:limit]
 
     # Find where it is target
-    target_data = skill.get_targets_data()
-    target_data = sorted(target_data, key=itemgetter(1), reverse=True)
+    target_data = sorted(skill.get_targets_data(), key=itemgetter(1), reverse=True)[:limit]
 
-    if limit:
-        target_data = target_data[:limit]
+    # If clicked...
+    clicked = request.args.get('clicked')
+    if clicked:
+        clicked_skill = Skill.query.filter_by(name=term).first()
+        clicked_target_data = sorted(clicked_skill.get_targets_data(), key=itemgetter(1), reverse=True)[:limit]
+        clicked_origins_data = sorted(clicked_skill.get_origins_data(), key=itemgetter(1), reverse=True)[:limit]
+        for o in clicked_origins_data:
+            td = {'source': o[0], 'target': clicked_skill.name, 'count': o[1]}
+            data.append(td)
+        for t in clicked_target_data:
+            td = {'source': clicked_skill.name, 'target': t[0], 'count': t[1]}
+            data.append(td)
 
-    data = []
-    for o in od:
+    for o in origin_data:
         td = {'source': o[0], 'target': skill.name, 'count': o[1]}
         data.append(td)
 
@@ -54,7 +59,7 @@ def data():
         td = {'source': skill.name, 'target': t[0], 'count': t[1]}
         data.append(td)
 
-    return jsonify({'dzta': data})
+    return jsonify({'data': data})
 
 
 
