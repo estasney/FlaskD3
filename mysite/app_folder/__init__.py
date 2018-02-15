0777a13c9efddebe0ccf2c518b0a342ce2ab360d
Eric Stasney <ericstasney@gmail.com>
2018-02-15 00:27:54 -0500

    directed graph

--- mysite/app_folder/__init__.py
+++ mysite/app_folder/__init__.py
@@ -2,12 +2,12 @@ from flask import Flask
 from site_config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask.ext.cache import Cache
+
 
 
 app_run = Flask(__name__)
 
-cache = Cache(app_run, config={'CACHE_TYPE': 'simple'})
+
 
 app_run.config.from_object(Config)
 db = SQLAlchemy(app_run)
